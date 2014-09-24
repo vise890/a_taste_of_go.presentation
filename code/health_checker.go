a88e163867c3b0d1d07b9b470f7928fbfc208920
Martino Visintin <mvisinti@thoughtworks.com>
2014-09-24 10:27:20 +0200

    add more urls for extra umpf

--- code/health_checker.go
+++ code/health_checker.go
@@ -17,7 +17,9 @@ func main() {
 	statusCodes := make(chan string)
 
 	var urls = []string{
-		"http://www.ubuntu.com/404", "http://www.mozilla.org", "http://www.eff.org",
+		"http://www.mozilla.org", "http://www.coursera.org", "http://www.eff.org",
+		"http://www.wc3.org", "http://www.torproject.org", "http://www.repubblica.it",
+		"http://www.ubuntu.com/404", "http://127.0.0.1:3999",
 	}
 
 	for _, url := range urls {
